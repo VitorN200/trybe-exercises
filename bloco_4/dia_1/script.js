3b9b1f2e55056c1e51c464bb1336d5df5f7109ae
Vitor Nunes <vitornunes200@gmail.com>
2020-10-23 17:37:41 -0300

    Exercicio 1 e 2

---
+++ bloco_4/dia_1/script.js
@@ -0,0 +1,29 @@
+let a = 2; 
+let b = 3;  
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;  
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
+
+// --------------------------------------
+
+let num1 = 15;
+let num2 = 20;
+let maior;
+
+if(num1 > num2){
+    maior = num1;
+}else{
+    maior = num2;
+}
+
+console.log(maior);
+
