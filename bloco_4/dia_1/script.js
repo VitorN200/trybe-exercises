cae9284e481d87b42e893ba6042075af9de50330
Vitor Nunes <vitornunes200@gmail.com>
2020-10-23 17:50:35 -0300

    exe 3 e 4 feitos

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -1,31 +1,57 @@
-let a = 2; 
-let b = 3;  
+// let a = 2; 
+// let b = 3;  
 
-let adicao = a + b;
-let subtracao = a - b;
-let multiplicacao = a * b;  
-let divisao = a / b;
-let modulo = a % b;
+// let adicao = a + b;
+// let subtracao = a - b;
+// let multiplicacao = a * b;  
+// let divisao = a / b;
+// let modulo = a % b;
 
-console.log(adicao);
-console.log(subtracao);
-console.log(multiplicacao);
-console.log(divisao);
-console.log(modulo);
+// console.log(adicao);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(modulo);
 
 // --------------------------------------
 
-let num1 = 15;
-let num2 = 20;
-let maior;
+// let num1 = 15;
+// let num2 = 20;
+// let maior;
 
-if(num1 > num2){
-    maior = num1;
-}else{
-    maior = num2;
-}
+// if(num1 > num2){
+    
+// }else{
+//     maior = num2;
+// }
 
-console.log(maior);
+// console.log(maior);
 
 // -------------------------------------------
 
+// let num1 = 10;
+// let num2 = 20;
+// let num3 = 30;
+// let maior;
+
+// if(num1 > num2 && num1 > num3){
+//     maior = num1;
+// }else if(num2 > num1 && num2 > num3){
+//     maior = num2;
+// }else{
+//     maior = num3;
+// }
+
+// console.log(maior);
+
+// -------------------------------
+
+let num = 0;
+
+if( num > 0){
+    console.log("positive")
+}else if(num < 0){
+    console.log("negative")
+}else{
+    console.log("zero")
+}
