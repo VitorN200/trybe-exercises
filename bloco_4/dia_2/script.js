8fe33099693452b7fe0072fb340a32736d99aa3b
Vitor Nunes <vitornunes200@gmail.com>
2020-10-26 18:30:20 -0300

    Exercicio 1 e 2

---
+++ bloco_4/dia_2/script.js
@@ -0,0 +1,18 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Ex 1
+
+// for( let index = 0; index < numbers.length; index++){
+
+//     console.log(numbers[index]);
+// }
+
+// Ex 2
+
+let soma = 0;
+for( let index = 0; index < numbers.length; index++){
+
+    soma += numbers[index]; 
+}
+
+console.log(soma);
